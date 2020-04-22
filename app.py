fe5afa33948559abf6fda0dc5c23c85fee4ed69e
Chris <christianOlmos@protonmail.com>
2020-04-22 22:03:42 +0000

    homework

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 todos = []
 stop = False
 
@@ -6,15 +8,27 @@ def get_todos():
     return todos
 
 def add_one_task(title):
-    # your code here
+    todos.append(title)
     pass
 
 def print_list():
     global todos
+    print ("\n \n  THINGS TO DO: \n")
+    lengthy =len(todos)
+    for i in range (0,lengthy):
+      print(str(i+1)+")"+ " "+ todos[i] + "\n")
+      sleep(0.4)
     pass
 
 def delete_task(number_to_delete):
-    # your code here
+    global todos
+    number_as_int = int(number_to_delete)
+    number_as_int = number_as_int - 1
+ #  deleted_item = int(todos[number_as_int])
+    print("... deleting item" +" "+ str(number_as_int+1))
+    del todos[number_as_int]
+    sleep(0.6)
+    print_list()
     pass
 
 def save_todos():
